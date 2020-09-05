4a16996b4a0983dc812ff78bcf332abcdf5cb7ee
Henrik Tunedal <tunedal@gmail.com>
2020-09-05 11:06:21 +0200

    Add Gradle task for building fat JAR

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.jvm.tasks.Jar
 
 plugins {
     kotlin("jvm") version "1.4.0"
@@ -15,4 +16,24 @@ dependencies {
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+// https://stackoverflow.com/questions/41794914
+val fatJar = task("fatJar", type = Jar::class) {
+    archiveBaseName.set("${project.name}-fat")
+    manifest {
+        attributes["Implementation-Title"] = "TBX Converter"
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = "MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map {
+        if (it.isDirectory) it else zipTree(it)
+    })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
